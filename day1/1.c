fd32a45a143d47e3b5981963912fa71722443531
kevinfiol <me@kevinfiol.com>
2022-12-09 20:32:12 -0500

    c

---
+++ day1/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, kevin!\n");  // Actually do the work here
+}
+
+// https://beej.us/guide/bgc/html/split/file-inputoutput.html#file-inputoutput
+// https://github.com/antirez/adventofcode2022/tree/main/day-1
+// https://archive.ph/u17jC#selection-625.0-625.8
